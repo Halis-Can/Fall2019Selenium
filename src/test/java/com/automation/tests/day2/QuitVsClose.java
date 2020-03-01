d7207db3a7bfb0c4e7af2b0ba8a6f3b1d71ca6ad
Halis-Can <haliscan@hotmail.com>
2020-03-01 16:17:22 -0500

    Created Basic Navigation Class modify

--- src/test/java/com/automation/tests/day2/QuitVsClose.java
+++ src/test/java/com/automation/tests/day2/QuitVsClose.java
@@ -1,7 +1,16 @@
 package com.automation.tests.day2;
 
-public class QuitVsClose {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class QuitVsClose {
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/new_tab");
+        Thread.sleep(3000);
+       driver.close();
+       //driver.quit();
     }
 }
