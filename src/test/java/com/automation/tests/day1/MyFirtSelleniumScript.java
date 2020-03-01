27fe9b64266c9a6f45695938100a8794e76e57ae
Halis-Can <haliscan@hotmail.com>
2020-03-01 12:40:02 -0500

    My First Selenium Script

---
+++ src/test/java/com/automation/tests/day1/MyFirtSelleniumScript.java
@@ -0,0 +1,15 @@
+package com.automation.tests.day1;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverInfo;
+
+public class MyFirtSelleniumScript {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
