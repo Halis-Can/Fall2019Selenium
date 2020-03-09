1cc8cbad8439310dec22e270669a09e30c1af50e
Halis-Can <haliscan@hotmail.com>
2020-03-09 11:20:48 -0400

    Day6 Dropdown and Alert

--- src/test/java/com/automation/tests/day1/MyFirtSelleniumScript.java
+++ src/test/java/com/automation/tests/day1/MyFirtSelleniumScript.java
@@ -8,8 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriverInfo;
 
 public class MyFirtSelleniumScript {
     public static void main(String[] args) {
+        //setup chromedriver
         WebDriverManager.chromedriver().setup();
+        //create chromedriver object
         ChromeDriver driver=new ChromeDriver();
+        //open some website
         driver.get("https://www.google.com/");
         //change name
     }
